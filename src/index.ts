6eb8af18b2a62adaaec63b4ed55779ff8f7f74ca
stevkim <stevenn.kkim@gmail.com>
2024-02-29 15:09:52 -0800

    Rollup used to include css

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import useMenuIcon from "./useMenuIcon";
 // export { default as MenuIcon } from "./MenuIcon";
 // export { default as useMenuIcon } from "./useMenuIcon";
 
-export default { MenuIcon, useMenuIcon };
+export { MenuIcon, useMenuIcon };
