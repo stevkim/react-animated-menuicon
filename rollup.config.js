2ff6f584db0ddfbff9415078943bcf844d98bbe5
stevkim <stevenn.kkim@gmail.com>
2024-03-01 13:16:05 -0800

    Added terser for minified bundles

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ const peerDepsExternal = require("rollup-plugin-peer-deps-external");
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const babel = require("@rollup/plugin-babel");
+const { default: terser } = require("@rollup/plugin-terser");
 
 module.exports = {
   input: "src/index.ts",
@@ -30,6 +31,7 @@ module.exports = {
     babel({
       babelHelpers: "bundled",
     }),
+    terser(),
   ],
   external: ["react", "react-dom"],
 };
